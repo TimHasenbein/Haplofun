b4ceea5232a3ef45a0ae4756bd99b1e196775ab8
Tim Hasenbein <66944893+TimHasenbein@users.noreply.github.com>
2026-07-21 15:04:18 +0200

    Add files via upload

---
+++ 00_app/app.R
@@ -0,0 +1,30 @@
+##**************************##
+##  Haplofun: app function  ##
+##**************************##
+## Project: haplofun
+## Last modification 07.2026
+## Creation: 2023
+library(shiny)
+library(vcfR)
+library(pegas)
+library(data.table)
+library(dplyr)
+library(ggdendro)
+library(ggplot2)
+library(shinycssloaders)
+library(graphics)
+library(shinyFeedback)
+library(tools)
+library(shinythemes)
+library(shinydashboard)
+library(RColorBrewer)
+library(reshape2)
+library(igvShiny)
+library(VariantAnnotation)
+library(bslib)
+source('./appUI.R')
+source('./appServer.R')
+source('./global.R') 
+
+# Run the application
+shinyApp(ui = ui, server = server)
